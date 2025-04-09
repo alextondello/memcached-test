216f441b85948bf9b83649b086450d85d7839d02
Alexandre Tondello <alexandre.tondello@gympass.com>
2025-04-09 18:32:35 -0300

    Create calculateDuration()

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 // Imports
 const Memcached = require("./memcached.js");
 // Variables
-const array = Array.from({ length: 1_00 }, (_, i) => i);
+const array = Array.from({ length: 1_000 }, (_, i) => i);
 const read = [];
 const write = [];
 
 // Functions
 function calculateAverage(array) {
-  if (array.length === 0) return 0;
-
+  if (!array || array.length === 0) return 0;
   const sum = array.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
   return sum / array.length;
+}
+
+function calculateDuration(hrtimeStart) {
+  const [seconds, nanoseconds] = process.hrtime(hrtimeStart);
+  const executionDurationMs = seconds * 1000 + nanoseconds / 1e6;
+  return executionDurationMs;
 }
 
 // Main execution
@@ -19,28 +24,18 @@ function calculateAverage(array) {
    * Read test
    */
   for (const i of array) {
-    const hrtime = process.hrtime();
-
+    const start = process.hrtime();
     await Memcached.setValue(`${i}`, "value", Math.random() * 10);
-
-    const [seconds, nanoseconds] = process.hrtime(hrtime);
-    const executionDurationMs = seconds * 1000 + nanoseconds / 1e6;
-
-    read.push(executionDurationMs);
+    read.push(calculateDuration(start));
   }
 
   /**
    * Write test
    */
   for (const i of array) {
-    const hrtime = process.hrtime();
-
+    const start = process.hrtime();
     await Memcached.getValue(`${i}`, "value");
-
-    const [seconds, nanoseconds] = process.hrtime(hrtime);
-    const executionDurationMs = seconds * 1000 + nanoseconds / 1e6;
-
-    write.push(executionDurationMs);
+    write.push(calculateDuration(start));
   }
 
   /**
